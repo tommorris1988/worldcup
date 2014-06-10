7f3071e1c8b74b4f8b3fc01addd775cd13d89ff8
tommorris1988 <tom@fiascodesign.co.uk>
2014-06-10 10:38:26 +0100

    Helper graphic, Landing page

--- wp-content/themes/Foundation/landing.php
+++ wp-content/themes/Foundation/landing.php
@@ -3,16 +3,19 @@
     <article class="content">
         <span class="icon-whistle clearfix"></span>
         <span class="icon-logo-white clearfix"></span>
-        <?php 
-        $page_id = 70;
-		$page_data = get_page( $page_id );
-		$content = apply_filters('the_content', $page_data->post_content);
-        if($content) :
+        <?php
+        global $post;
+        $page = get_posts( array( 'slug' => 'landing', 'post_type'=>'page' ) );
+
+        if ( $page ) {
+            $content = apply_filters('the_content', $page[0]->post_content);
             echo $content;
-        	echo '<a class="cta wide close-landing" href="#">Let\'s Play</a>';
-        else :
+            echo '<a class="cta wide close-landing" href="#">Let\'s Play</a>';
+        } else {
             echo '<p class="center">Sorry, this page is under construction..</p>';
-        endif; ?>
+        }
+
+        ?>
     </article>
 
 </header>
